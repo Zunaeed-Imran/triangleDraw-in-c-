b02c6e55e5caba75fa95366b33373ba89d8bf3fc
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-29 15:29:21 +0600

    TriangleDraw in C++

---
+++ drow2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  cout <<"    1"<<endl;
+  cout <<"   1 "<< ""<<"1"<<endl;
+  cout <<"  1 "<< "  "<<"1"<<endl;
+  cout <<" 1 "<< "    "<<"1"<<endl;
+  cout << "1" << "1" << "1" << "1" << "1" << "1" << "1" << "1" << "1" << endl;
+
+  cout << endl;
+  cout << endl;
+
+  cout << "     1" << endl << "    1 1" << endl << "   1   1" << endl << "  1     1" << endl << " 111111111" << endl;
+
+  return 0;
+}
